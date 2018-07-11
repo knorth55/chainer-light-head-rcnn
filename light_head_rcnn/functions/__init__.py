7e6c174972d7e0cd07d163707af22f908b139760
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-07-11 20:17:16 +0900

    add psroi_max_align_2d and psroi_average_align_2d

--- light_head_rcnn/functions/__init__.py
+++ light_head_rcnn/functions/__init__.py
@@ -1,2 +1,7 @@
 from light_head_rcnn.functions.psroi_align_2d import psroi_align_2d  # NOQA
 from light_head_rcnn.functions.psroi_align_2d import PSROIAlign2D  # NOQA
+from light_head_rcnn.functions.psroi_max_align_2d import psroi_max_align_2d  # NOQA
+from light_head_rcnn.functions.psroi_max_align_2d import PSROIMaxAlign2D  # NOQA
+
+psroi_average_align_2d = psroi_align_2d
+PSROIAverageAlign2D = PSROIAlign2D
