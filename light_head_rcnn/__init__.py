86f01e000c98bba49901eac493095d26a4a358e9
Shingo Kitagawa <shingogo@hotmail.co.jp>
2018-07-13 18:22:04 +0900

    add ManualScheduler

--- light_head_rcnn/__init__.py
+++ light_head_rcnn/__init__.py
@@ -1,2 +1,3 @@
+from light_head_rcnn import extensions  # NOQA
 from light_head_rcnn import functions  # NOQA
 from light_head_rcnn import links  # NOQA
---
+++ light_head_rcnn/extensions/__init__.py
@@ -0,0 +1,1 @@
+from light_head_rcnn.extensions.manual_scheduler import ManualScheduler  # NOQA
